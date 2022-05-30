24943fe0531b93e44caaa85af28ef1677df390f5
elm_niconico <elmgameinfo@gmail.com>
2022-05-31 00:29:50 +0900

    fix: スコアボードの表示処理を再度修正しました。

--- src/scenes/ScoreBoard.ts
+++ src/scenes/ScoreBoard.ts
@@ -1,24 +1,29 @@
 import {IScene} from "./changeScene";
 
 import {createScene, Scene} from "../util/entityHelper";
-import {GameMainParameterObject} from "../parameterObject";
-import {window} from "../main";
+import {GameMainParameterObject, RPGAtsumaruWindow} from "../parameterObject";
+
 
 export class ScoreBoard implements IScene {
     createScene(param: GameMainParameterObject): Scene {
         return createScene({game: g.game})
     }
 
-    async execute(scene: Scene, param: GameMainParameterObject): Promise<void> {
-        await showScoreboardIfAtsumaru(param)
+    async execute(scene: Scene, param: GameMainParameterObject, window: RPGAtsumaruWindow): Promise<void> {
+        await showScoreboardIfAtsumaru(param, window)
     }
 }
 
-const showScoreboardIfAtsumaru = async (param: GameMainParameterObject) => {
-    // ゲームアツマール環境であればランキングを表示します
-    if (param.isAtsumaru) {
-        const boardId = 1;
-        await window.RPGAtsumaru.experimental.scoreboards.setRecord(boardId, g.game.vars.gameState.score);
-        window.RPGAtsumaru.experimental.scoreboards.display(boardId);
+const showScoreboardIfAtsumaru = async (param: GameMainParameterObject, window: RPGAtsumaruWindow) => {
+    try {
+        // ゲームアツマール環境であればランキングを表示します
+
+        if (param.isAtsumaru) {
+            const boardId = 1;
+            await window.RPGAtsumaru.scoreboards.setRecord(boardId, g.game.vars.gameState.score);
+            window.RPGAtsumaru.scoreboards.display(boardId);
+        }
+    } catch (e) {
+        console.error(e)
     }
 }
--- src/scenes/changeScene.ts
+++ src/scenes/changeScene.ts
@@ -2,13 +2,18 @@ import {GameMainParameterObject} from "@akashic/akashic-engine";
 import {pushScene, Scene} from "../util/entityHelper";
 import {ScoreBoard} from "./ScoreBoard";
 import {Game} from "./Game";
+import {RPGAtsumaruWindow} from "../parameterObject";
 
-export const changeScene = (param: GameMainParameterObject, type: SceneType) => new Promise<void>((resolve) => {
+export const changeScene = (
+    type: SceneType,
+    param: GameMainParameterObject,
+    window: RPGAtsumaruWindow
+) => new Promise<void>((resolve) => {
     const sceneExe = fact(type)
     const scene = sceneExe.createScene(param)
     scene.onLoad.add(() => {
         (async () => {
-            await sceneExe.execute(scene, param)
+            await sceneExe.execute(scene, param, window)
 
             resolve()
         })()
@@ -29,5 +34,5 @@ export type SceneType = "Game" | "Scoreboards"
 export interface IScene {
     createScene(param: GameMainParameterObject): Scene
 
-    execute(scene: Scene, param: GameMainParameterObject): Promise<void>
+    execute(scene: Scene, param: GameMainParameterObject, window: RPGAtsumaruWindow): Promise<void>
 }
